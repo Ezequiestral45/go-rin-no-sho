2fa34d1093bc770bc6f0cdc742b7b7a6bf4c330f
Ezequiestral45 <ezequiel.thon@gmail.com>
2026-07-27 18:52:13 -0400

    Initial commit

---
+++ src/main/java/net/ezeq/gorinnosho/GoRinNoShoClient.java
@@ -0,0 +1,10 @@
+package net.ezeq.gorinnosho;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoRinNoShoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ezeq/gorinnosho/GoRinNoShoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ezeq.gorinnosho;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GoRinNoShoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
